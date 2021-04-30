47806b41e9b037283f0dc4b8f7e027198e77cf9f
guylurieg <83456783+guylurieg@users.noreply.github.com>
2021-04-30 15:55:05 +0300

    Update healthcheck.sh
    Added Comment

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while [ 1 ]; 
 do
-	HTTP=$(curl -LI http://localhost/app -o /dev/null -w '%{http_code}\n' -s)
+	HTTP=$(curl -LI http://localhost/app -o /dev/null -w '%{http_code}\n' -s) #Curls And Extracts Only HTTP Result
 if [ $HTTP == "200" ];
 then  
 echo "Endpoint Up"
